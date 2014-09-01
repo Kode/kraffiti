00f9a67a16c0c66b5f4b236b434cec3e1e1efaca
Robert Konrad <robert@ktx-software.com>
2014-09-01 04:40:18 +0200

    Accept tiny variations when searching for color keys.

--- Sources/Preprocessor.cpp
+++ Sources/Preprocessor.cpp
@@ -1,22 +1,28 @@
 #include <imagew.h>
 #include "Preprocessor.h"
 
-unsigned toValue(unsigned char* pixel) {
-	unsigned char r = pixel[0]; 
-	unsigned char g = pixel[1];
-	unsigned char b = pixel[2];
-	unsigned char a = pixel[3];
-	return (r << 24) | (g << 16) | (b << 8) | a;
-}
-
 void transparent(iw_context* context, unsigned color) {
 	iw_image image;
 	iw_get_output_image(context, &image);
 
 	for (int y = image.height - 1; y >= 0; --y) {
 		for (int x = 0; x < image.width; ++x) {
-			unsigned value = toValue(&image.pixels[y * image.bpr + x * 4]);
-			if (value == color) {
+			unsigned char* pixel = &image.pixels[y * image.bpr + x * 4];
+
+			unsigned char r = pixel[0];
+			unsigned char g = pixel[1];
+			unsigned char b = pixel[2];
+			unsigned char a = pixel[3];
+
+			unsigned char red = (color & 0xff000000) >> 24;
+			unsigned char green = (color & 0xff0000) >> 16;
+			unsigned char blue = (color & 0xff00) >> 8;
+
+			bool isred = r == red || r == red - 1 || r == red + 1;
+			bool isgreen = g == green || g == green - 1 || g == green + 1;
+			bool isblue = b == blue || b == blue - 1 || b == blue + 1;
+
+			if (isred && isgreen && isblue) {
 				image.pixels[y * image.bpr + x * 4 + 3] = 0;
 			}
 		}
