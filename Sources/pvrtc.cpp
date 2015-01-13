5d023aaf6eec5f649b45b5de2371ff9e26565d19
Robert Konrad <robert@ktx-software.com>
2015-01-13 17:21:16 +0100

    pvrtc improvements.

--- Sources/pvrtc.cpp
+++ Sources/pvrtc.cpp
@@ -42,12 +42,58 @@ namespace {
 
 		iw_process_image(context);
 	}
+	
+	int pow(int pow) {
+		int ret = 1;
+		for (int i = 0; i < pow; ++i) ret *= 2;
+		return ret;
+	}
+	
+	int getPower2(int i) {
+		for (int power = 0; ; ++power)
+			if (pow(power) >= i) return pow(power);
+	}
 
 	void writePVRTC(iw_image* image, const char* filename) {
-		pvrtexture::CPVRTextureHeader header(pvrtexture::PVRStandard8PixelType.PixelTypeID, image->width, image->height);
-		pvrtexture::CPVRTexture texture(header, image->pixels);
+		int w = getPower2(image->width);
+		int h = getPower2(image->height);
+		unsigned char* pixels = new unsigned char[w * h * 4];
+		for (int y = 0; y < h; ++y) for (int x = 0; x < w; ++x) {
+			pixels[y * w * 4 + x * 4 + 0] = 0;
+			pixels[y * w * 4 + x * 4 + 1] = 0;
+			pixels[y * w * 4 + x * 4 + 2] = 0;
+			pixels[y * w * 4 + x * 4 + 3] = 0;
+		}
+		for (int y = 0; y < image->height; ++y) for (int x = 0; x < image->width; ++x) {
+			pixels[y * w * 4 + x * 4 + 0] = image->pixels[y * image->bpr + x * 4 + 0];
+			pixels[y * w * 4 + x * 4 + 1] = image->pixels[y * image->bpr + x * 4 + 1];
+			pixels[y * w * 4 + x * 4 + 2] = image->pixels[y * image->bpr + x * 4 + 2];
+			pixels[y * w * 4 + x * 4 + 3] = image->pixels[y * image->bpr + x * 4 + 3];
+		}
+		pvrtexture::CPVRTextureHeader header(pvrtexture::PVRStandard8PixelType.PixelTypeID, w, h);
+		pvrtexture::CPVRTexture texture(header, pixels);
 		pvrtexture::Transcode(texture, ePVRTPF_PVRTCI_4bpp_RGBA, ePVRTVarTypeUnsignedByteNorm, ePVRTCSpacelRGB);
+		
+		MetaDataBlock widthBlock;
+		widthBlock.DevFOURCC = 0;
+		widthBlock.u32Key = 0;
+		widthBlock.u32DataSize = 4;
+		widthBlock.Data = new unsigned char[4];
+		unsigned char* ww = (unsigned char*)&image->width;
+		for (int i = 0; i < 4; ++i) widthBlock.Data[i] = ww[i];
+		texture.addMetaData(widthBlock);
+		
+		MetaDataBlock heightBlock;
+		heightBlock.DevFOURCC = 1;
+		heightBlock.u32Key = 0;
+		heightBlock.u32DataSize = 4;
+		heightBlock.Data = new unsigned char[4];
+		unsigned char* hh = (unsigned char*)&image->height;
+		for (int i = 0; i < 4; ++i) heightBlock.Data[i] = hh[i];
+		texture.addMetaData(heightBlock);
+		
 		texture.saveFile(filename);
+		delete[] pixels;
 	}
 
 	int imax(int a, int b) {
@@ -58,7 +104,7 @@ namespace {
 void pvrtc(iw_context* context, const char* filename) {
 	//int w = iw_get_value(context, IW_VAL_INPUT_WIDTH);
 	//int h = iw_get_value(context, IW_VAL_INPUT_HEIGHT);
-	//scale(context, imax(w, h), imax(w, h));
+	//scale(context, getPower2(w), getPower2(h));
 	iw_image img;
 	iw_get_output_image(context, &img);
 	writePVRTC(&img, filename);
